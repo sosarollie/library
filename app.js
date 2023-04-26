2436bbddea66bd4ca16a87c2de7b1fb2c6619a1c
18praise <jesussosarollie1812@gmail.com>
2023-04-25 22:38:42 -0300

    add dynamic input for number pages.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const container = document.querySelector(".container"); //grid container.
 const submit = document.querySelector(".submit");
+const inProgressRadio = document.getElementById("in_progress"); 
 
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read, cover) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
+  this.cover = cover;
 }
 
 function addBookToLibrary() {
@@ -16,7 +19,7 @@ function addBookToLibrary() {
   const read = document.querySelector('input[name="read"]:checked').value;
   const cover = document.getElementById('cover').value;
 
-  const newBook = new Book(title, author, pages);
+  const newBook = new Book(title, author, pages, read, cover);
 
   myLibrary.push(newBook);
   showBook(newBook);
@@ -28,7 +31,7 @@ function showBook(book) {
     let figcaption = document.createElement("figcaption");
     let ul = document.createElement("ul");
 
-    cardContainer.innerHTML = "<img src='./images/meditations.jpg'>";
+    cardContainer.innerHTML = book.cover;
 
     for(const key in book) {
       let card = document.createElement("li");
@@ -43,7 +46,7 @@ function showBook(book) {
           card.classList.add("author");
           break;
         case "pages":
-          card.innerHTML = book.pages;
+          card.innerHTML = book.read + book.pages;
           card.classList.add("status_display");
           break;
       }
@@ -54,20 +57,36 @@ function showBook(book) {
     container.appendChild(cardContainer);
 }
 
-function openform() {
+function openForm() {
   const popUp = document.querySelector('.popUp');
   popUp.classList.add('active');
 }
 
-function closeform() {
+function closeForm() {
   const popUp = document.querySelector('.popUp');
   popUp.classList.remove('active');
 }
 
+function openNumberPages() {
+  const newInput = document.createElement('input');
+  newInput.type = 'number';
+  const newLabel = document.createElement('label');
+  const progressLabel = document.getElementById('in_progress_label');
+
+  newLabel.innerHTML = " | I'm at page: ";
+
+  progressLabel.insertAdjacentElement('afterend', newLabel);
+  newLabel.insertAdjacentElement('afterend', newInput);
+}
+
 submit.addEventListener('click', function(event){
   event.preventDefault();
-  closeform();
+  closeForm();
   addBookToLibrary();
+})
+
+inProgressRadio.addEventListener('click', function(event){
+  openNumberPages();
 })
 
 window.onload = (event) =>{
