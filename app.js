0412605165693fe079f7bc9c8fdd967977586e62
18praise <jesussosarollie1812@gmail.com>
2023-04-29 18:47:14 -0300

    fix image upload

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".container"); //grid container.
 const submit = document.querySelector(".submit");
 const inProgressRadio = document.getElementById("in_progress"); 
+const fileInput = document.getElementById('cover');
+
+let selectedImg;
 
 let myLibrary = [];
 
@@ -12,18 +15,21 @@ function Book(title, author, pages, read, cover) {
   this.cover = cover;
 }
 
+fileInput.onchange = () => {
+  selectedImg = fileInput.files[0];
+  console.log(selectedImg);
+}
+
 function addBookToLibrary() {
   const title = document.getElementById('title_input').value;
   const author = document.getElementById('author_input').value;
   const pages = document.getElementById('pages_input').value;
   const read = document.querySelector('input[name="read"]:checked').value;
-  const cover = document.getElementById('cover').value;
 
-  const newBook = new Book(title, author, pages, read, cover);
+  const newBook = new Book(title, author, pages, read, selectedImg);
 
   myLibrary.push(newBook);
   showBook(newBook);
-  console.log(myLibrary);
 }
 
 function showBook(book) {
@@ -32,11 +38,10 @@ function showBook(book) {
     let figcaption = document.createElement("figcaption");
     let ul = document.createElement("ul");
 
-    image.setAttribute("src", book.cover);
+    image.setAttribute("src", URL.createObjectURL(book.cover));
 
     for(const key in book) {
       let card = document.createElement("li");
-      console.log(key);
       switch(key) {
         case "title":
           card.innerHTML = book.title;
@@ -47,7 +52,7 @@ function showBook(book) {
           card.classList.add("author");
           break;
         case "pages":
-          card.innerHTML = book.read + "(" + book.read + book.pages + ")";
+          card.innerHTML = book.read + "(" + book.pages + ")";
           card.classList.add("status_display");
           break;
       }
@@ -75,6 +80,7 @@ submit.addEventListener('click', function(event){
   closeForm();
   addBookToLibrary();
 })
+
 
 window.onload = (event) =>{
   const figures = document.querySelectorAll("figure");
