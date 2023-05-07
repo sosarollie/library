27b0cee0388fe90719fa299635c8f4d778a35e54
18praise <jesussosarollie1812@gmail.com>
2023-05-06 21:22:23 -0300

    run prettier.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const form = document.querySelector("form");
 const inputs = document.querySelectorAll(".inputs");
 const submit = document.querySelector(".submit");
 const edit = document.querySelector(".edit");
-const inProgressRadio = document.getElementById("in_progress"); 
-const fileInput = document.getElementById('cover');
+const inProgressRadio = document.getElementById("in_progress");
+const fileInput = document.getElementById("cover");
 const figures = document.querySelectorAll("figure");
 let delButtons = document.getElementsByClassName("fa-trash-can");
 console.log(delButtons);
@@ -25,74 +25,86 @@ function Book(title, author, pagesProgress, pagesTotal, read, cover) {
 
 fileInput.onchange = () => {
   selectedImg = fileInput.files[0];
-}
+};
 
 function addBookToLibrary() {
-
-  const title = document.getElementById('title_input').value;
-  const author = document.getElementById('author_input').value;
-  const pagesProgress = document.getElementById('pages_progress_input').value;
-  const pagesTotal = document.getElementById('pages_total_input').value;
+  const title = document.getElementById("title_input").value;
+  const author = document.getElementById("author_input").value;
+  const pagesProgress = document.getElementById("pages_progress_input").value;
+  const pagesTotal = document.getElementById("pages_total_input").value;
   const read = document.querySelector('input[name="read"]:checked').value;
 
-  const newBook = new Book(title, author, pagesProgress, pagesTotal, read, selectedImg);
-  
+  const newBook = new Book(
+    title,
+    author,
+    pagesProgress,
+    pagesTotal,
+    read,
+    selectedImg
+  );
+
   showBook(newBook);
   moveAddButton();
   console.log(myLibrary);
 }
 
 function showBook(book) {
-    const cardContainer = document.createElement("figure");
-    const image = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    const ul = document.createElement("ul");
-    const buttonContainer = document.createElement("div");
-    const delButton = document.createElement("button");
-    const editButton = document.createElement("button");
+  const cardContainer = document.createElement("figure");
+  const image = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  const ul = document.createElement("ul");
+  const buttonContainer = document.createElement("div");
+  const delButton = document.createElement("button");
+  const editButton = document.createElement("button");
 
-    if (book.cover != "template"){ 
-      image.setAttribute("src", URL.createObjectURL(book.cover));
-    } else image.setAttribute("src", "./images/coverTemplate.jpg");
-    
-    delButton.classList.add("fa-solid", "fa-trash-can");
-    editButton.classList.add("fa-sharp", "fa-solid", "fa-pen-to-square");
-    buttonContainer.classList.add("buttonContainer");
+  if (book.cover != "template") {
+    image.setAttribute("src", URL.createObjectURL(book.cover));
+  } else image.setAttribute("src", "./images/coverTemplate.jpg");
 
-    for(const key in book) {
-      let card = document.createElement("li");
-      switch(key) {
-        case "title":
-          card.innerHTML = book.title;
-          card.classList.add("title");
-          break;
-        case "author":
-          card.innerHTML = book.author;
-          card.classList.add("author");
-          break;
-        case "pagesProgress":
-          if (book.read === "In Progress" && book.pagesProgress <= book.pagesTotal){
-            card.innerHTML = book.read + `(${book.pagesProgress}/${book.pagesTotal})`;  
-          } else card.innerHTML = book.read;
-          card.classList.add("status_display");
-          break;
-      }
-      ul.appendChild(card);
+  delButton.classList.add("fa-solid", "fa-trash-can");
+  editButton.classList.add("fa-sharp", "fa-solid", "fa-pen-to-square");
+  buttonContainer.classList.add("buttonContainer");
+
+  for (const key in book) {
+    let card = document.createElement("li");
+    switch (key) {
+      case "title":
+        card.innerHTML = book.title;
+        card.classList.add("title");
+        break;
+      case "author":
+        card.innerHTML = book.author;
+        card.classList.add("author");
+        break;
+      case "pagesProgress":
+        if (
+          book.read === "In Progress" &&
+          book.pagesProgress <= book.pagesTotal
+        ) {
+          card.innerHTML =
+            book.read + `(${book.pagesProgress}/${book.pagesTotal})`;
+        } else card.innerHTML = book.read;
+        card.classList.add("status_display");
+        break;
     }
+    ul.appendChild(card);
+  }
 
-    figcaption.appendChild(ul);
-    cardContainer.appendChild(image);
-    cardContainer.appendChild(figcaption);
-    buttonContainer.appendChild(editButton);
-    buttonContainer.appendChild(delButton);
-    cardContainer.appendChild(buttonContainer);
-    container.appendChild(cardContainer); //displays book on the page.
-    myLibrary.push(cardContainer); //adds book to the array.
+  figcaption.appendChild(ul);
+  cardContainer.appendChild(image);
+  cardContainer.appendChild(figcaption);
+  buttonContainer.appendChild(editButton);
+  buttonContainer.appendChild(delButton);
+  cardContainer.appendChild(buttonContainer);
+  container.appendChild(cardContainer); //displays book on the page.
+  myLibrary.push(cardContainer); //adds book to the array.
 }
 
-function editForm(book){
-  const pagesProgress = document.getElementById('pages_progress_input_edit').value;
-  const pagesTotal = document.getElementById('pages_total_input_edit').value;
+function editForm(book) {
+  const pagesProgress = document.getElementById(
+    "pages_progress_input_edit"
+  ).value;
+  const pagesTotal = document.getElementById("pages_total_input_edit").value;
   const read = document.querySelector('input[name="read_edit"]:checked').value;
 
   console.log(book);
@@ -104,14 +116,13 @@ function editForm(book){
   const ulChild = ul.childNodes;
   const readStatus = ulChild[5];
 
-
-  if (book.read === "In Progress" && pagesProgress <= pagesTotal){
+  if (book.read === "In Progress" && pagesProgress <= pagesTotal) {
     readStatus.innerHTML = `${read} (${pagesProgress}/${pagesTotal})`;
   } else readStatus.innerHTML = read;
 }
 
 function moveAddButton() {
-  const addBookFig =  myLibrary.at(-2);
+  const addBookFig = myLibrary.at(-2);
   myLibrary.splice(-2, 1);
   myLibrary.push(addBookFig);
   container.appendChild(addBookFig);
@@ -119,27 +130,27 @@ function moveAddButton() {
 }
 
 function openForm() {
-  const popUp = document.getElementById('add');
-  popUp.classList.add('active');
+  const popUp = document.getElementById("add");
+  popUp.classList.add("active");
 }
 
 function closeForm() {
-  const popUp = document.getElementById('add');
-  popUp.classList.remove('active');
+  const popUp = document.getElementById("add");
+  popUp.classList.remove("active");
 }
 
 function openEditForm() {
-  const popUp = document.getElementById('editForm');
-  popUp.classList.add('active');
+  const popUp = document.getElementById("editForm");
+  popUp.classList.add("active");
 }
 
 function closeEditForm() {
-  const popUp = document.getElementById('editForm');
-  popUp.classList.remove('active');
+  const popUp = document.getElementById("editForm");
+  popUp.classList.remove("active");
 }
 
-function updateIndex(){
-  for (let i = 0; i< myLibrary.length; i++){
+function updateIndex() {
+  for (let i = 0; i < myLibrary.length; i++) {
     myLibrary[i].dataset.index = i;
   }
 }
@@ -149,44 +160,42 @@ function deleteElement(index) {
   myLibrary.splice(index, 1);
 }
 
-submit.addEventListener('click', function(event){
+submit.addEventListener("click", function (event) {
   event.preventDefault();
   closeForm();
   addBookToLibrary();
   updateIndex();
 });
 
-edit.addEventListener('click', function(event){
+edit.addEventListener("click", function (event) {
   event.preventDefault();
   closeEditForm();
   editForm(currentBook);
-})
+});
 
-document.addEventListener("click", function(e){
+document.addEventListener("click", function (e) {
   const target = e.target.closest(".fa-trash-can");
-  
-  if(target){
+
+  if (target) {
     const bookContainer = target.parentNode.parentNode;
     console.log(bookContainer);
     deleteElement(bookContainer.dataset.index);
     updateIndex();
   }
-
 });
 
-document.addEventListener("click", function(e){
+document.addEventListener("click", function (e) {
   const target = e.target.closest(".fa-pen-to-square");
-  
-  if(target){
+
+  if (target) {
     openEditForm();
     const book = target.parentNode.parentNode;
     currentBook = book;
   }
-
 });
 
 window.onload = (event) => {
-  figures.forEach(figure=> {
+  figures.forEach((figure) => {
     myLibrary.push(figure);
   });
   updateIndex();
